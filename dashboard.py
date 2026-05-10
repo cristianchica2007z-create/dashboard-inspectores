bd00d7dffd7daaf36b4fa09e0db02df791874d21
Cristian Chica <cristianchica2007z@gmail.com>
2026-05-10 00:17:23 -0500

    style: adjust logo vertical position and container height for improved dashboard layout

--- dashboard.py
+++ dashboard.py
@@ -658,7 +658,7 @@ if st.session_state.usuario is None:
         .circle-logo {{
             position: absolute;
             left: 50%;
-            top: 50%;
+            top: 280px; /* Forzado más abajo para centrado vertical real */
             transform: translate(-50%, -50%); /* Centrado absoluto perfecto respecto a toda la tarjeta */
             width: 160px; /* Logo más grande */
             height: 160px;
@@ -760,7 +760,7 @@ if st.session_state.usuario is None:
             }}, 1000);
         </script>
         """
-        components.html(html_code, height=450)
+        components.html(html_code, height=600)
         
     st.stop()
 
